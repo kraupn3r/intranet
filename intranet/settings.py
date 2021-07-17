7bc9f677f730c689f6cf4ca8cc267d47fdb34a9b
m <m.pyzowski@o2.pl>
2021-07-17 11:29:46 +0200

    move secret key to an ignored file

--- intranet/settings.py
+++ intranet/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from intranet.restconf.main import *
+from secretkey.py import secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_DIR = os.path.join(BASE_DIR,'media/')
@@ -22,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates/')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%_m9qcrf!%h@p_cm6=@ozp+ku4h7oz_vfk4g-c+i@vd+nazv8d'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
